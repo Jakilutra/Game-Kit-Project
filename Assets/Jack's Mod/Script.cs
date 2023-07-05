9fa93aba6f0c4b8ce8c47a3a643fa12ab00cad10
Jakilutra <jakilutra@gmail.com>
2023-07-05 04:25:43 +0100

    Making Levels

--- Assets/Jack's Mod/Script.cs
+++ Assets/Jack's Mod/Script.cs
@@ -26,7 +26,9 @@ public class Script : MonoBehaviour
 
     private int currentJump = 0, selector = 0, countDown = 0, jumpsUsed;
 
-    private bool togglePaused = false;
+    private bool togglePaused = false, clear = false;
+
+    private float levelTime;
 
     // Start is called before the first frame update
     void Start()
@@ -41,9 +43,9 @@ public class Script : MonoBehaviour
 
         jumpConfig = new Dictionary<string, int[]>()
         {
-            { "Level 1", new int[] { 1, 2, 3, 4, 5, 6, 7 } },
-            { "Level 2", new int[] { 1, 2, 1, 2, 1, 2, 1 } },
-            { "Level 3", new int[] { 2, 3, 4, 5, 7, 8, 9 } },
+            { "Level 1", new int[] { 2, 2, 2, 2, 4, 4, 4 } },
+            { "Level 2", new int[] { 2, 2, 2, 4, 4, 4, 4 } },
+            { "Level 3", new int[] { 2, 2, 2, 2, 2, 5, 5 } },
             { "Level 4", new int[] { 7, 7, 7, 7, 7, 7, 7 } },
             { "Level 5", new int[] { 3, 4, 5, 6, 7, 8, 9 } },
         };
@@ -75,19 +77,23 @@ public class Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // Restart Button
         if (Input.GetKeyDown(KeyCode.P))
         {
             SceneManager.LoadScene(currentScene);
         }
 
-       // Y and U toggle;
+        // Increment Level Time Statistic
+
+        levelTime += Time.deltaTime;
+
+        // Y and U toggle;
 
         if (Input.GetKey(KeyCode.Y) && currentJumpConfig.Length > 0 && !togglePaused)
         {
             togglePaused = true;
             Invoke("UndoTogglePause", 0.3f);
-            currentJump = currentJump == 0 ? currentJumpConfig.Length-1 : currentJump - 1;
+            currentJump = currentJump == 0 ? currentJumpConfig.Length - 1 : currentJump - 1;
             playerScript.jumpSpeed = currentJumpConfig[currentJump] * 10;
             selector = currentJump * 3;
             UpdateOverlay();
@@ -97,7 +103,7 @@ public class Script : MonoBehaviour
         {
             togglePaused = true;
             Invoke("UndoTogglePause", 0.3f);
-            currentJump = currentJump == currentJumpConfig.Length-1 ? 0 : currentJump + 1;
+            currentJump = currentJump == currentJumpConfig.Length - 1 ? 0 : currentJump + 1;
             playerScript.jumpSpeed = currentJumpConfig[currentJump] * 10;
             selector = currentJump * 3;
             UpdateOverlay();
@@ -117,14 +123,14 @@ public class Script : MonoBehaviour
                 playerScript.jumpSpeed = 0;
                 Invoke("SetJumpSpeed", 0.3f);
             }
-.
+
             if (currentJumpConfig.Length > 0 && playerCollider.IsTouchingLayers(LayerMask.GetMask("Platform")) & !togglePaused)
             {
                 // Statistic
 
                 jumpsUsed++;
 
-                // Time Buffer to stop jump powers being used up too quickly in succession.
+                // Time Buffer to stop jump powers being used up.
 
                 togglePaused = true;
                 Invoke("UndoTogglePause", 0.6f);
@@ -146,7 +152,7 @@ public class Script : MonoBehaviour
 
                 if (currentJumpConfig.Length == 0)
                 {
-                    countDown = 6;
+                    countDown = 4;
                     InvokeRepeating("restartCount", 0f, 1f);
                 }
             }
@@ -161,13 +167,30 @@ public class Script : MonoBehaviour
 
         // Highlighted Jump Power
 
-        var jumpPowers = currentJumpConfig.Length > 0 ? jumpString.Substring(0, Math.Max(selector,0)) + "<color=green>" + jumpString[selector] + "</color>" + jumpString.Substring(selector+1): "<color=blue>Restarting in " + countDown + " seconds</color>";
+        string jumpPowers = currentJumpConfig.Length > 0 ? jumpString.Substring(0, Math.Max(selector, 0)) + "<color=green>" + jumpString[selector] + "</color>" + jumpString.Substring(selector + 1) : "<color=blue>Restarting in " + countDown + " seconds</color>";
 
-        textComponent.text = "Select your jump power:\n"
+        float minutes = Mathf.FloorToInt((levelTime % 3600f) / 60f),
+            seconds = Mathf.FloorToInt(levelTime % 60f),
+            ms = Mathf.FloorToInt(levelTime * 100f) % 100f;
+
+        string formattedTime = "Time: " + string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, ms);
+
+        if (!clear) {
+            textComponent.text = "Select your jump power:\n"
                     + "\n"
                     + jumpPowers + "\n"
                     + "\n"
                     + "Type U and Y to toggle";
+        }
+        else {
+            textComponent.text = "<color=green>Jumps Taken: " + jumpsUsed + "</color>\n"
+                                + "\n"
+                                + "<color=red>Health Left: " + playerScript.damageable.CurrentHealth + "</color>\n"
+                                + "\n"
+                                + formattedTime + "\n"
+                                + "\n"
+                                + "<color=blue>Next Level Coming up!</color>";
+        }
     }
 
     public void UndoTogglePause()
@@ -188,5 +211,27 @@ public class Script : MonoBehaviour
         {
             SceneManager.LoadScene(currentScene);
         }
+    }
+
+    public void LoadNextLevel()
+    {
+        int nextLevel;
+        if (int.TryParse(currentScene.Substring(6), out nextLevel))
+        {
+            SceneManager.LoadScene("Level " + (nextLevel + 1).ToString());
+        }
+
+    }
+
+    public void LevelClear ()
+    {
+        if (IsInvoking("restartCount"))
+        {
+            CancelInvoke("restartCount");
+        }
+        clear = true;
+        playerScript.maxSpeed = 0;
+        UpdateOverlay();
+        Invoke("LoadNextLevel", 5);
     }
 }
